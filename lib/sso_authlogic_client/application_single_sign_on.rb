e9fc8f6f32e9cf5100fc6e97cdd4a573eca636c7
Justin Derrek Van Eaton <jvaneat@iit.edu>
2018-03-28 09:54:15 -0600

    update redirect to include key

--- lib/sso_authlogic_client/application_single_sign_on.rb
+++ lib/sso_authlogic_client/application_single_sign_on.rb
@@ -8,7 +8,7 @@ module SsoAuthlogicClient::ApplicationSingleSignOn
   def check_remote_login
     return unless login_service
     session[:remote_login_allowed] = true
-    redirect_to login_service['login_service_base_url'] and return if login_service['force_login_on_remote_sso_service']
+    redirect_to "#{login_service['login_service_base_url']}?client_key=#{login_service['access_key_id']}" and return if login_service['force_login_on_remote_sso_service']
     unless ping(login_service['ping_domain'], login_service['ping_port'])
       session[:remote_login_allowed] = false
     end
